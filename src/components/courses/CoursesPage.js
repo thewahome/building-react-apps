633f67ab479e9d6cbfca557839bc91b9d4802beb
Charles Wahome <thewahome.cw@gmail.com>
2019-03-19 10:12:30 +0300

    add mapDispatchToProps manually

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
   state = {
@@ -15,8 +18,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    debugger
-    alert(this.state.course.title)
+    this.props.dispatch(courseActions.createCourse(this.state.course))
   }
 
   render () {
@@ -26,9 +28,31 @@ class CoursesPage extends React.Component {
         <h4>Add Course</h4>
         <input type='text' onChange={this.handleChange} value={this.state.course.title} />
         <input type='submit' value='Save'/>
+        {
+          this.props.courses.map(course => (
+            <div key={course.title}>{course.title}</div>
+          ))
+        }
       </form>
       );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);

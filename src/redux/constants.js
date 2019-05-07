eebf24fac6cce45c727d2f0a585e46314bc67435
Charles Wahome <thewahome@users.noreply.github.com>
2019-05-07 17:40:53 +0300

    add thunk, move magic strings to constants

---
+++ src/redux/constants.js
@@ -0,0 +1,3 @@
+// Actions
+export const CREATE_COURSE = 'CREATE_COURSE';
+export const LOAD_COURSES_SUCCESS = 'LOAD_COURSES_SUCCESS';
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,10 @@
+import * as types from '../constants';
 export default function courseReducer(state = [], action){
     switch (action.type) {
-        case 'CREATE_COURSE':
+        case types.CREATE_COURSE:
             return [...state, {...action.course}];
+        case types.LOAD_COURSES_SUCCESS:
+            return action.courses;
         default:
             return state;
     }

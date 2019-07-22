dd6bd9c2014ef0a066241aa94e16184b6ee3c043
Charles Wahome <thewahome.cw@gmail.com>
2019-07-22 12:57:22 +0300

    add reusable components

---
+++ src/components/common/SelectInput.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SelectInput = ({
+  name,
+  label,
+  onChange,
+  defaultOption,
+  value,
+  options,
+  error
+}) => {
+  let wrapperClass = 'form-group';
+  if (error && error.length > 0) {
+    wrapperClass += ' has-error';
+  }
+  return(
+    <div className={wrapperClass}>
+      <label htmlFor={name}>{label}</label>
+      <div className='field'>
+        <select name={name} className='form-control' value={value} onChange={onChange}>
+          <option value=''>{defaultOption}</option>
+          {options.map(option => {
+            return(
+              <option key={option.value} value={option.value}>{option.text}</option>
+            )
+          })}
+        </select>
+
+        <input type='text'  />
+        {error  && <div className='alert alert-danger'> {error} </div>}
+      </div>
+    </div>
+  )
+};
+
+SelectInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  defaultOption: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  options: PropTypes.arrayOf([PropTypes.object]),
+  error: PropTypes.object
+}
+
+export default SelectInput;

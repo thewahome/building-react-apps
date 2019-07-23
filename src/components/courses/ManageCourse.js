276efce03e4ba8dd2ef42438ebef94c77980db9a
Charles Wahome <thewahome.cw@gmail.com>
2019-07-23 12:21:34 +0300

    client side validation

--- src/components/courses/ManageCourse.js
+++ src/components/courses/ManageCourse.js
@@ -46,8 +46,21 @@ function ManageCourse({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = 'Title is required';
+    if (!authorId) errors.author = 'Author is required';
+    if (!category) errors.category = 'Category is required';
+
+    setError(errors);
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     setSaving(true)
     saveCourse(course).then(() => {
       toast.success('Course saved');

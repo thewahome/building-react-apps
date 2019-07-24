c15daadbd20f9a94d85dd9a4eb7a99e8be4080c7
Charles Wahome <thewahome.cw@gmail.com>
2019-07-24 15:12:14 +0300

    testing thunks (api calls)

--- src/tests/services/courseActions.test.js
+++ src/tests/services/courseActions.test.js
@@ -1,6 +1,41 @@
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+
 import * as courseActions from '../../redux/actions/courseActions';
 import * as types from '../../redux/constants';
 import { courses } from '../../../tools/mockData';
+
+//test an async function
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async actions', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  describe('load courses thunk', () => {
+    it('should create BEGIN_API_CALL and LOAD_COURSES_SUCCESS when loading courses', () => {
+      fetchMock.mock('*', {
+        body: courses,
+        headers: { 'content-type': 'application/json' }
+      });
+
+      const expectedActions = [
+        { type: types.BEGIN_API_CALL },
+        { type: types.LOAD_COURSES_SUCCESS, courses },
+      ];
+
+      const store = mockStore({ courses: [] });
+      return store.dispatch(courseActions.loadCourses()).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      })
+    })
+  })
+
+})
+
 
 describe('create course success', () => {
   it('should create CREATE_COURSE_SUCCESS action', () => {
